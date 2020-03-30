5a5b98c17f0feac7fcc6268109a07fb83b94e15f
Golden Rockefeller <rockefeg@oregonstate.edu>
2020-03-30 10:10:32 -0700

    Make package 'install' instead of 'develop'

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         packages=find_namespace_packages("src"),
         package_dir={'': 'src'},
         install_requires=['cython', 'numpy'],
-        script_args = ["develop"],
+        package_data={"": ["*.pxd", "*.pyx", "*.pyxbld"]},
+        script_args = ["install"],
         namespace_packages = [namespace],
         python_requires = ">=3.3")
